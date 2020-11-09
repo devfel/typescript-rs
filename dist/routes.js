b6ad5ae26972ae10f0694443ba5df64d9a47b6e1
Felizardo <devfelizardo@gmail.com>
2020-11-09 03:51:18 -0300

    adding typification and createUser file to the project

--- dist/routes.js
+++ dist/routes.js
@@ -1,7 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helloWorld(request, response) {
-    return response.json({ message: "Hello World" });
+    var user = CreateUser_1.default({
+        email: "luiz@gmail.com",
+        password: "1234",
+        techs: ["Node.js", "ReactJS", "React Native", { title: "JavaScript", exp: 100 }],
+    });
+    console.log(user.email);
+    return response.json({ message: "Hello World" + user.password });
 }
 exports.helloWorld = helloWorld;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "luiz@gmail.com",
+    password: "1234",
+    techs: ["Node.js", "ReactJS", "React Native", { title: "JavaScript", exp: 100 }],
+  });
+
+  console.log(user.email);
+
   return response.json({ message: "Hello World" });
 }
